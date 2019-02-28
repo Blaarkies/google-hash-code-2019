8995ad0c043c55e5cff932c5f826daa56decfd8d
Pierre Roux <pierre.roux@entelect.co.za>
2019-02-28 19:52:38 +0200

    Add input files

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 import Group.Group;
 
-import java.awt.*;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
