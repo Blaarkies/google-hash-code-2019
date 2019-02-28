ca53b81b98cd817b565675c75d928fa0d53c7cb6
Renette Ros <renetteR@discovery.co.za>
2019-02-28 19:37:51 +0200

    IO init

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,30 @@
 import java.awt.*;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        solve("problem1");
+    }
 
+    private static void solve(final String name) throws IOException {
+        List<String> input = Files.readAllLines(Paths.get("input/" + name + ".in"));
 
-        List<String> results = Files.readAllLines(Paths.get("input/problem1.txt"));
+//        Scanner scanner = new Scanner(new FileInputStream("input.txt"));
+//
+//        int testCases = scanner.nextInt();
+//        for (int t = 0; t < testCases; t++) {
+//            int r = scanner.nextInt();
+//            int c = scanner.nextInt();
+//            int k = scanner.nextInt();
+//
+//        }
 
-
-        Scanner scanner = new Scanner(new FileInputStream("input.txt"));
-
-
-        int testCases = scanner.nextInt();
-        for (int t = 0; t < testCases; t++) {
-            int r = scanner.nextInt();
-            int c = scanner.nextInt();
-            int k = scanner.nextInt();
-
-        }
-
-        Files.write(Paths.get("output/problem1.txt"), results);
-
+        Files.write(Paths.get("output/problem1.out"), new Solution(input).solve());
     }
 
     public static Integer distance(Point start, Point end) {
---
+++ src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+
+    private final List<String> input;
+
+    public Solution(List<String> input) {
+        this.input = input;
+    }
+
+    public List<String> solve() {
+        return Collections.emptyList();
+    }
+}
